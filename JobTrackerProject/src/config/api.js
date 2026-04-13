99a71371cb2bfa43a72ab35dac43645077d2e98c
Sanmoy Pal <s.p.sanmoy@gmail.com>
2026-04-13 20:10:10 +0530

    url fixed

--- JobTrackerProject/src/config/api.js
+++ JobTrackerProject/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://jobtrackerproject-q72g.onrender.com",
+  baseURL: "https://jobtrackerproject-q72g.onrender.com/api",
   //baseURL: "http://localhost:5000/api",
 });
 
